1ca02ad5ad130e39a8475351ee3fa4d112a88f0e
inchanS <song@inchan.dev>
2024-03-22 20:45:50 +0900

    fix: github auto release에 맞게 info.plist 수정  - submodule의 url을 ssh주소에서 https 주소로 변경  - github Action으로 파일 생성시 파일명 수정

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm ./NaverSearch.alfredworkflow
+rm ./NaverSearchPlus.alfredworkflow
 cd workflow
 sed "s/{{VERSION_INFO}}/${GITHUB_REF##*/v}/g" < info.plist > info.plist.bak
 mv info.plist.bak info.plist
-zip -r ../NaverSearch.alfredworkflow .
+zip -r ../NaverSearchPlus.alfredworkflow .
